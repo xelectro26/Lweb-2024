e63f96e20b941fb44f6c3dae211c73a81cc3b6f1
Denis Boitor <159322249+xelectro26@users.noreply.github.com>
2024-09-23 17:11:20 +0200

    Delete 1-homework directory

--- 1-homework/res/js/script.js
+++
@@ -1,9 +0,0 @@
-function openMenu() {
-    document.getElementById("sideMenu").style.width = "250px";
-    // Scorri verso il contenuto principale
-    document.querySelector('.container').scrollIntoView({ behavior: 'smooth' });
-}
-
-function closeMenu() {
-    document.getElementById("sideMenu").style.width = "0";
-}
