59329d562f064cf2901869f6143c9e8dc5f2f503
Denis Boitor <159322249+xelectro26@users.noreply.github.com>
2024-11-27 15:31:52 +0100

    Delete 2-homework directory

--- 2-homework/res/PHP/log-out.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset(); // Rimuove tutte le variabili di sessione
-session_destroy(); // Distrugge la sessione
-
-header('Location: ../../Homepage.php'); // Reindirizza alla pagina principale
-exit();
-?>
